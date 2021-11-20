69f8226a482307814027d055958cdc3eddee3bc2
almustaphamuha98@gmail.com <Almustapha.Ibrahim@norrenberger.local>
2021-11-21 00:45:59 +0100

    Added Spinner component:

---
+++ src/components/ui/Spinner.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+    100% { transform: rotate(360deg) }
+`;
+
+const dash = keyframes`
+    0% {
+        stroke-dasharray: 1, 150;
+        stroke-dashoffset: 0;
+    }
+    50% {
+        stroke-dasharray: 90, 150;
+        stroke-dashoffset: -35;
+    }
+    50% {
+        stroke-dasharray: 90, 150;
+        stroke-dashoffset: -124;
+    }
+`;
+
+const Svg = styled.svg`
+    width: 30px;
+    height: 30px;
+    animation: ${rotate} 2s linear infinite;
+
+    circle {
+        fill: none;
+        stroke-width: 5px;
+        stroke-linecap: round;
+        animation: ${dash} 1.7s ease-in-out infinite;
+    }
+`;
+
+
+const Spinner = props => {
+    const { strokeColor } = props;
+    const stroke = strokeColor ? strokeColor : '#fff';
+
+    return (
+        <Svg viewBox="0 0 50 50">
+            <circle
+                stroke={stroke}
+                cx="25"
+                cy="25"
+                r="20"
+            >
+            </circle>
+        </Svg>
+    );
+};
+
+Spinner.propTypes = {
+    strokeColor: PropTypes.string
+};
+
+export default Spinner;
