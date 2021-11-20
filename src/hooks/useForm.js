b7f8c3b4a14fdde16d5149e521e4ce69b514caea
almustaphamuha98@gmail.com <Almustapha.Ibrahim@norrenberger.local>
2021-11-20 21:26:22 +0100

    completed useForm hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,2 +1,37 @@
-import { useReducer } from 'react';
-import controlChange from '../util/forms/controlChange';
+import { useReducer, useCallback } from 'react';
+import controlChange from 'util/forms/controlChange';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE': return controlChange(state, action.payload);
+        case 'RESET': return { ...state, ...action.payload };
+        default: throw Error('useForm.js: invalid action type');
+    };
+};
+
+const useForm = initialState => {
+    const [form, dispatch] = useReducer(reducer, initialState);
+
+    const changeHandler = useCallback((value, control) => {
+        dispatch({
+            type: 'CHANGE',
+            payload: { control, value }
+        });
+    }, []);
+
+    const resetForm = () => dispatch({
+        type: 'RESET',
+        payload: initialState
+    });
+
+    const controls = Object
+        .keys(form.controls)
+        .map(key => form.controls[key]);
+
+
+    return { form, changeHandler, controls, resetForm };
+};
+
+
+export default useForm;
+
